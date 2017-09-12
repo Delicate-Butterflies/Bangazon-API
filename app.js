de80b3c39690c8cbcc9f61ae4490a2e750534539
Megan B <brown.m.j@outlook.com>
2017-09-12 15:56:15 -0500

    set up app.js with express, route, error, and port

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+let routes = require('./routes/');
+require('dotenv').config();
+
+app.use('/api/v1/', routes);
+
+app.use( (req, res, next) => {
+	let err = new Error('Not found!');
+	err.status = 404;
+	next();
+});
+
+app.use( (err, req, res, next) => {
+	res.status(err.status || 500);
+	res.json({
+		message: 'Unsuccessful',
+		err: err
+	});
+});
+
+let port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
