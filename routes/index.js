db93c273afdfbf85e13c50944378c5eeb7646f10
Megan B <brown.m.j@outlook.com>
2017-09-13 11:38:51 -0500

    routing on index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,41 @@
-"use strict";
+'use strict';
 
 const { Router } = require('express');
 const router = Router();
+
+function makeRoutes(tableName) {
+	router.use(require(`./api/v1/${tableName}`));
+	router.use(require(`./api/v1/${tableName}/:id`));
+	router.use(require(`./api/v1/${tableName}/add-new`));
+	router.use(require(`./api/v1/${tableName}/:id/edit`));
+}
+
+function makeRoutesWithDelete(tableName) {
+	router.use(require(`./api/v1/${tableName}`));
+	router.use(require(`./api/v1/${tableName}/:id`));
+	router.use(require(`./api/v1/${tableName}/add-new`));
+	router.use(require(`./api/v1/${tableName}/:id/edit`));
+	router.use(require(`./api/v1/${tableName}/:id/delete`));
+}
+
+// employee (GET, POST, PUT)
+makeRoutes("employee");
+console.log(makeRoutes("employee"));
+// department (GET, POST, PUT)
+makeRoutes("department");
+// computer (GET, POST, PUT, DELETE)
+makeRoutesWithDelete("computer");
+// training program (GET, POST, PUT, DELETE)
+makeRoutesWithDelete("training-program");
+// user (GET, POST, PUT)
+makeRoutes("user");
+// product type (GET, POST, PUT, DELETE)
+makeRoutesWithDelete("product-type");
+// product (GET, POST, PUT, DELETE)
+makeRoutes("product");
+// order (GET, POST, PUT, DELETE)
+makeRoutesWithDelete("order");
+// payment type (GET, POST, PUT, DELETE)
+makeRoutesWithDelete("payment-type");
 
 module.exports = router;
