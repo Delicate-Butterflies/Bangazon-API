6db878521c2dbf43c36c4d2cbce93859c754d066
Megan B <brown.m.j@outlook.com>
2017-09-14 10:05:56 -0500

    ctrl - get all employees

--- controllers/Employee-Ctrl.js
+++ controllers/Employee-Ctrl.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const { } = require('../models/Employee');
+
+module.exports.getEmployees = (req, res, next) => {
+	dbGetAllEmployees()
+	.then( (employees) => {
+		res.status(200).json(employees);
+	})
+	.catch( (err) => {
+		next(err);
+	});
+};
+
