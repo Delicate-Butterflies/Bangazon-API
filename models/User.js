84362040ea55c48ca386177b9d614291cbcf1444
Arwa <arwa.it15@gmail.com>
2017-09-14 10:08:55 -0500

    getAllUser method

--- models/User.js
+++ models/User.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../bangazon.sqlite');
+
+module.exports = {
+  dbGetAllUsers: () => {
+    return new Promise( (resolve, reject) => {
+      db.all(`SELECT * FROM users`, (err, userData) => {
+        if(err) {
+          console.log("Error: ", err.toString());
+          reject(err);
+        }
+        else
+          resolve(userData);
+      })
+    })
+  }
+}
