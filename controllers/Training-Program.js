56990f68898971d7da564cd5dea54e0c7a112b8e
Sam <samb283@gmail.com>
2017-09-14 14:25:54 -0500

    post functionality working

--- controllers/Training-Program.js
+++ controllers/Training-Program.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const { dbPostTrainingProgram } = require('../models/Training-Program');
+
+module.exports.postTrainingProgram = (req, res, next) => {
+    dbPostTrainingProgram(req.body)
+        .then((response) => {
+            res.status(200).json(response);
+        })
+        .catch((err) => {
+            next(err);
+        });
+};
