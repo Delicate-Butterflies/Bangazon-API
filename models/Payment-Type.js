7d1162abde384d3969572c249dfe8b695d8cb5fb
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-09-15 15:33:18 -0500

    removed returns on promise rejects in payment type model

--- models/Payment-Type.js
+++ models/Payment-Type.js
@@ -6,7 +6,7 @@ const db = new sqlite3.Database('./db/bangazon.sqlite');
 module.exports.dbGetAllPaymentTypes = () => {
   return new Promise( (resolve, reject) => {
     db.all(`SELECT * FROM payment_types`, (err, allPaymentTypesData) => {
-      if(err) return reject(err);
+      if(err) reject(err);
       resolve(allPaymentTypesData);
     });
   });
@@ -15,7 +15,7 @@ module.exports.dbGetAllPaymentTypes = () => {
 module.exports.dbGetOnePaymentType = (id) => {
   return new Promise((resolve, reject) => {
     db.get(`SELECT * FROM payment_types WHERE id = ${id}`, (err, singlePaymentTypeData) => {
-      if(err) return reject(err);
+      if(err) reject(err);
       resolve(singlePaymentTypeData);
     });
   });
