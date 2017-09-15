2ca8482a047af45a16cf792de11c3a23b78991d2
Sam <samb283@gmail.com>
2017-09-15 09:51:32 -0500

    training programs all requests handled

--- controllers/Training-Program.js
+++ controllers/Training-Program.js
@@ -1,13 +1,53 @@
 'use strict';
 
-const { dbPostTrainingProgram } = require('../models/Training-Program');
+const { dbGetAllTrainingPrograms, dbGetOneTrainingProgram, dbPostTrainingProgram, dbDeleteOneTrainingProgram, dbPutTrainingProgram } = require('../models/Training-Program');
+
+module.exports.getTrainingPrograms = (req, res, next) => {
+  dbGetAllTrainingPrograms()
+  .then( (trainingPrograms) => {
+    res.status(200).json(trainingPrograms);
+  })
+  .catch( (err) => {
+    next(err);
+  });
+};
+
+module.exports.getSingleTrainingProgram = ({params: {id}}, res, next) =>{
+  dbGetOneTrainingProgram(id)
+  .then( (trainingProgram) => {
+    res.status(200).json(trainingProgram);
+  })
+  .catch( (err) => {
+    next(err);
+  });
+};
 
 module.exports.postTrainingProgram = (req, res, next) => {
-    dbPostTrainingProgram(req.body)
-        .then((response) => {
-            res.status(200).json(response);
-        })
-        .catch((err) => {
-            next(err);
-        });
+  dbPostTrainingProgram(req.body)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports.deleteTrainingProgram = ({params: {id}}, res, next) => {
+  dbDeleteOneTrainingProgram(id)
+  .then((deleteConfirmation) => {
+    res.status(200).json(deleteConfirmation);
+  })
+  .catch( (err) => {
+    next(err);
+  });
+};
+
+module.exports.putTrainingProgram = (req, res, next) => {
+  let id = req.params.id;
+  dbPutTrainingProgram(req, id)
+  .then( (editedTrainingProgram) => {
+    res.status(200).json(editedTrainingProgram);
+  })
+  .catch( (err) =>
+  next(err));
 };
--- models/Training-Program.js
+++ models/Training-Program.js
@@ -4,6 +4,23 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/bangazon.sqlite');
 
 module.exports = {
+    dbGetAllTrainingPrograms: () => {
+        return new Promise( (resolve, reject) => {
+          db.all(`SELECT * FROM training_programs`, (err, trainingData) => {
+            if(err) return reject(err);
+            resolve(trainingData);
+          });
+        });
+      },
+      dbGetOneTrainingProgram: (id) => {
+        return new Promise( (resolve, reject) => {
+          db.get(`SELECT * FROM training_programs
+                  WHERE id = ${id}`, (err, trainingData) => {
+            if(err) return reject(err);
+            resolve(trainingData);
+          });
+        });
+      },
     dbPostTrainingProgram: (newTrainingProgram) => {
         return new Promise((resolve, reject) => {
             let { start_date, end_date, max_attendance, title } = newTrainingProgram;
@@ -14,15 +31,32 @@ module.exports = {
             });
         });
     },
-    //     getOne: (id) => {
-    //         return new Promise((resolve, reject) => {
-    //             db.get(`SELECT shows.*, directors.name AS director
-    //               FROM shows
-    //               JOIN directors ON director_id = directors.dir_id
-    //               WHERE show_id = ${id}`, (err, show) => {
-    //                 if (err) return reject(err);
-    //                 resolve(show);
-    //             });
-    //         });
-    //     }
+    dbDeleteOneTrainingProgram: (id) => {
+        return new Promise((resolve, reject) => {
+          db.run(`DELETE FROM training_programs WHERE id = ${id}`, function(err) {
+            if(err) return reject(err);
+            resolve({message: "delete successful", rows_deleted: this.changes});
+          });
+        });
+      },
+      dbPutTrainingProgram: (req, training_program_id) => {
+        let training_program = req.body;
+        return new Promise( (resolve, reject) => {
+          let query = `UPDATE training_programs SET `;
+          let keys = (Object.keys(training_program));
+          keys.forEach( (key) => {
+            query += `"${key}" = "${training_program[key]}",`;
+          });
+          query = query.slice(0,-1);
+          query += ` WHERE id = ${training_program_id}`;
+          db.run(query, function(err) {
+            if(err) {
+              reject(err);
+            }
+            else {
+              resolve({message: "training_program updated", rows_deleted: this.changes});
+            }
+          });
+        });
+      }
 };
