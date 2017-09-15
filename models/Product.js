4934ac2e2ea30a0b8d0db09454512016e5a12f44
Sam <samb283@gmail.com>
2017-09-15 11:45:43 -0500

    WIP routing set up properly - still need to check functionality

--- models/Product.js
+++ models/Product.js
@@ -1,2 +1,64 @@
 'use strict';
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/bangazon.sqlite');
+
+module.exports.dbGetAllProducts = () => {
+  return new Promise( (resolve, reject) => {
+    db.all(`SELECT * FROM products`, (err, productdata) => {
+      if(err) return reject(err);
+      resolve(productdata);
+    });
+  });
+};
+
+module.exports.dbGetOneTrainingProgram = (id) => {
+  return new Promise( (resolve, reject) => {
+    db.get(`SELECT * FROM products
+            WHERE id = ${id}`, (err, productdata) => {
+      if(err) return reject(err);
+      resolve(productdata);
+    });
+  });
+};
+
+module.exports.dbPostTrainingProgram = (newProduct) => {
+  return new Promise((resolve, reject) => {
+      let { type_id, price, title, description, original_quantity, seller_user_id } = newProduct;
+      db.run(`INSERT INTO products(product_type_id, price, title, description, original_quantity, seller_user_id)
+      VALUES('${type_id}', '${price}', '${title}', '${description}', '${original_quantity}', '${seller_user_id}')`, (err) => {
+          if (err) return reject(err);
+          resolve("New field inserted");
+      });
+  });
+};
+
+module.exports.dbDeleteOneTrainingProgram = (id) => {
+  return new Promise((resolve, reject) => {
+    db.run(`DELETE FROM products WHERE id = ${id}`, function(err) {
+      if(err) return reject(err);
+      resolve({message: "delete successful", rows_deleted: this.changes});
+    });
+  });
+};
+
+module.exports.dbPutTrainingProgram = (req, product_id) => {
+  let product = req.body;
+  return new Promise( (resolve, reject) => {
+    let query = `UPDATE products SET `;
+    let keys = (Object.keys(product));
+    keys.forEach( (key) => {
+      query += `"${key}" = "${product[key]}",`;
+    });
+    query = query.slice(0,-1);
+    query += ` WHERE id = ${product_id}`;
+    db.run(query, function(err) {
+      if(err) {
+        reject(err);
+      }
+      else {
+        resolve({message: "product updated", rows_deleted: this.changes});
+      }
+    });
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,8 @@ router.use(require('./employees'));
 router.use(require('./computers'));
 router.use(require('./users'));
 router.use(require('./training-programs'));
-
 // router.use(require('./product-types'));
-// router.use(require('./products'));
+router.use(require('./products'));
 // router.use(require('./orders'));
 // router.use(require('./payment-types'));
 
