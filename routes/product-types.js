8951d381d69d206fc7c401e5a1b378a66b41b4af
Arwa <arwa.it15@gmail.com>
2017-09-15 12:59:33 -0500

    change path name to plural

--- routes/product-types.js
+++ routes/product-types.js
@@ -5,10 +5,10 @@ const router = Router();
 
 const { getSingleProductType, getProductType, postProductType, putProductType, deleteProductType } = require('../controllers/Product-Type-Ctrl');
 
-router.get('/product-type', getProductType);
-router.get('/product-type/:id', getSingleProductType);
-router.post('/product-type', postProductType);
-router.put('/product-type/:id', putProductType);
-router.delete('/product-type/:id', deleteProductType);
+router.get('/product-types', getProductType);
+router.get('/product-types/:id', getSingleProductType);
+router.post('/product-types', postProductType);
+router.put('/product-types/:id', putProductType);
+router.delete('/product-types/:id', deleteProductType);
 
 module.exports = router;
