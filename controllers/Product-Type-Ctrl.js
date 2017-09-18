0de67123c9779ddbc99f744cb002c471f23f0fea
Sam <samb283@gmail.com>
2017-09-18 10:10:21 -0500

    cleaned up whitespace, removed an extra 'use strict', and adjusted indentaion left

--- controllers/Product-Type-Ctrl.js
+++ controllers/Product-Type-Ctrl.js
@@ -1,7 +1,5 @@
 'use strict';
 
-'use strict';
-
 const { dbGetAllProductType, dbGetOneProductType, dbPostProductType, dbPutProductType, dbDeleteProductType  } = require('../models/Product-Type.js');
 
 module.exports.getProductType = (req, res, next) => {
@@ -9,44 +7,44 @@ module.exports.getProductType = (req, res, next) => {
   .then( (ProductTypeData) => {
     res.status(200).json(ProductTypeData);
   })
-  .catch( (err) => 
+  .catch( (err) =>
     next(err));
   };
 
-  module.exports.getSingleProductType = ({params: {id}}, res, next) => {
-    dbGetOneProductType(id)
-    .then( (oneProductTypeData) => {
-      res.status(200).json(oneProductTypeData);
-    })
-    .catch( (err) => 
-    next(err));
-  };
+module.exports.getSingleProductType = ({params: {id}}, res, next) => {
+  dbGetOneProductType(id)
+  .then( (oneProductTypeData) => {
+    res.status(200).json(oneProductTypeData);
+  })
+  .catch( (err) =>
+  next(err));
+};
 
-  module.exports.postProductType = ( req, res, next) => {
-    let id = req.params.id;
-    dbPostProductType(req)
-    .then( (newProductTypeData) => {
-      res.status(200).json(newProductTypeData);
-    })
-    .catch( (err) => 
-    next(err));
-  }
-  module.exports.putProductType = ( req, res, next) => {
-    let id = req.params.id;
-    dbPutProductType(req, id)
-    .then( (editedProductTypeData) => {
-      res.status(200).json(editedProductTypeData);
-    })
-    .catch( (err) => 
-    next(err));
-  }
-  module.exports.deleteProductType = ( req, res, next) => {
-    let id = req.params.id;
-    console.log("id :", id);
-    dbDeleteProductType(id)
-    .then( (deletedProductTypeData) => {
-      res.status(200).json(deletedProductTypeData);
-    })
-    .catch( (err) => 
-    next(err));
-  }
+module.exports.postProductType = ( req, res, next) => {
+  let id = req.params.id;
+  dbPostProductType(req)
+  .then( (newProductTypeData) => {
+    res.status(200).json(newProductTypeData);
+  })
+  .catch( (err) =>
+  next(err));
+};
+module.exports.putProductType = ( req, res, next) => {
+  let id = req.params.id;
+  dbPutProductType(req, id)
+  .then( (editedProductTypeData) => {
+    res.status(200).json(editedProductTypeData);
+  })
+  .catch( (err) =>
+  next(err));
+};
+module.exports.deleteProductType = ( req, res, next) => {
+  let id = req.params.id;
+  console.log("id :", id);
+  dbDeleteProductType(id)
+  .then( (deletedProductTypeData) => {
+    res.status(200).json(deletedProductTypeData);
+  })
+  .catch( (err) =>
+  next(err));
+};
