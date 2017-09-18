46f38815d6ca5d3479a4d17607caaef2f0207b8d
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-09-18 09:36:12 -0500

    added post to payment types route

--- routes/payment-types.js
+++ routes/payment-types.js
@@ -5,11 +5,12 @@ const router = Router();
 
 const { getPaymentTypes,
         getSinglePaymentType,
-        deletePaymentType } = require('../controllers/Payment-Type-Ctrl');
+        deletePaymentType,
+        postPaymentType } = require('../controllers/Payment-Type-Ctrl');
 
 router.get('/payment-types', getPaymentTypes);
 router.get('/payment-types/:id', getSinglePaymentType);
-// router.post('/payment-type', postPaymentType);
+router.post('/payment-types', postPaymentType);
 // router.put('/payment-type/:id', putPaymentType);
 router.delete('/payment-types/:id', deletePaymentType);
 
